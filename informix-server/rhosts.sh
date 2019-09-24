f44590b907f12d00f77e55ef808c1d47afae5a6e
Darin Tracy <DARIN.TRACY@PROD.HCLPNP.COM>
2019-09-23 17:29:31 -0700

    updates

--- informix-server/rhosts.sh
+++ informix-server/rhosts.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 echo "client.composekernellab_default" > ~informix/.rhosts
-
-#sudo apt-get install -y gcc
--- informix-server/setup.sh
+++ informix-server/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo touch /tmp/jcfile
 sudo apt-get install -y gcc
